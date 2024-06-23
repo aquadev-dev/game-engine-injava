f66a89678099f1862602ad1bd767f2e568813227
aquadev__ <nathan.boucher@aquadev.org>
2024-06-23 18:46:53 +0100

    First Commit

---
+++ src/main/java/org/AquaDev/GameEngine/Main.java
@@ -0,0 +1,10 @@
+package org.AquaDev.GameEngine;
+
+import org.AquaDev.GameEngine.jade.Window;
+
+public class Main {
+    public static void main(String[] args) {
+        Window window = Window.get();
+        window.run();
+    }
+}
